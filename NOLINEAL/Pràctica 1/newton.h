a2277f5056804bde01853f2567c4a5f105274402
Gerard Lahuerta Martín <80009126+Gerard-Lahuerta@users.noreply.github.com>
2023-02-14 19:16:58 +0100

    Delete NOLINEAL directory

--- NOLINEAL/Pràctica 1/newton.h
+++
@@ -1,8 +0,0 @@
-#ifndef _NEWTON_H_
-#define _NEWTON_H_
-
-#include <stdio.h>
-#include "func.h"
-int newton(double);
-
-#endif // _NEWTON_H_
--- NOLINEAL/Pràctica 1/options.mk
+++
@@ -1,8 +0,0 @@
-# File to be included from the makefile
-INCLUDEPATH="/usr/local/gsl-2.7/include"
-LIBPATH="/usr/local/gsl-2.7/lib"
-DELETE=rm   # rm per Linux o MacOS
-
-
-
-
